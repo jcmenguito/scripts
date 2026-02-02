876e1b0355aefaa27089f4388b8a7eac6376a1b2
jcmenguito <jcmenguito@gmail.com>
2026-02-02 15:58:36 +0800

    Update wallpaper.sh

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-cd ~/Pictures/Wallpapers/Wallpaper-Bank/swww/
+
+# change ~/Pictures/Wallpapers if you keep your wallpapers in another
+# directory.
 while : ; do
-	for file in $(ls *.{png,jpg,jpeg} | shuf); do
+	for file in $(find ~/Pictures/Wallpapers -type f -iregex ".*\.\(jpg\|png\)$" | shuf); do
 		swww img "$file" --transition-type random
 		sleep 1m
 	done
 done
-
