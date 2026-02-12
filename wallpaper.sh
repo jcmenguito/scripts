bad944e2f3a0f3ff761eea3d138003077bc22083
jcmenguito <jcmenguito@gmail.com>
2026-02-12 09:27:36 +0800

    Update wallpaper.sh
    Added a wait routine for the swww-daemon socket

--- wallpaper.sh
+++ wallpaper.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# change ~/Pictures/Wallpapers if you keep your wallpapers in another
-# directory.
+# Wait for the swww socket to exist. Please check the actual name of the swww-daemon socket in your system
+while [ ! -S "$XDG_RUNTIME_DIR/${WAYLAND_DISPLAY}-swww-daemon..sock" ]; do
+    sleep 0.1
+done
+
+# Optional: Extra check to ensure daemon is responding
+until swww query &>/dev/null; do
+    sleep 0.1
+done
+
+# Start of wallpaper setting routine
 while : ; do
 	for file in $(find ~/Pictures/Wallpapers -type f -iregex ".*\.\(jpg\|png\)$" | shuf); do
 		swww img "$file" --transition-type random
